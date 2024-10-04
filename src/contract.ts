eb8a2af01330e3174299b19eb7708d14032b0330
Paulo Endoh <paulo.endoh@gmail.com>
2024-10-04 10:58:39 -0300

    second commit

---
+++ src/contract.ts
@@ -0,0 +1,47 @@
+import { initContract } from '@ts-rest/core';
+import { z } from 'zod';
+
+const PostSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  body: z.string(),
+});
+
+export type Post = z.infer<typeof PostSchema>;
+
+const c = initContract();
+
+export const postContract = c.router({
+  createPost: {
+    method: 'POST',
+    path: '/posts',
+    responses: {
+      201: PostSchema,
+    },
+    body: z.object({
+      title: z.string(),
+      body: z.string(),
+    }),
+    summary: 'Create a post',
+  },
+  getPost: {
+    method: 'GET',
+    path: `/posts/:id`,
+    responses: {
+      200: PostSchema,
+      404: z.null(),
+    },
+    summary: 'Get a post by id',
+  },
+  getAllPosts: {
+    method: 'GET',
+    path: '/posts',
+    responses: {
+      200: z.array(PostSchema),
+    },
+  },
+});
+
+export const contract = c.router({
+  posts: postContract,
+});
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { generateOpenApi } from '@ts-rest/open-api';
+import { AppModule } from './app.module';
+import { contract } from './contract';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const document = generateOpenApi(
+    contract,
+    {
+      info: {
+        title: 'My API',
+        version: '1.0.0',
+      },
+    },
+    {
+      setOperationId: true,
+    },
+  );
+
+  SwaggerModule.setup('/swagger', app, document, {
+    yamlDocumentUrl: '/swagger.yaml',
+  });
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  await app.listen(3123);
+}
+bootstrap();
