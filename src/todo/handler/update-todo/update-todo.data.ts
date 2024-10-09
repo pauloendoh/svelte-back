cb86e1aab8b83f6a51cccb65b637880b5b656eec
Paulo Endoh <paulo.endoh@gmail.com>
2024-10-08 23:36:28 -0300

    ferramenta de TODO basica backend em nest + front em svelte

--- src/todo/handler/update-todo/update-todo.data.ts
+++ src/todo/handler/update-todo/update-todo.data.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const updateTodoDataSchema = z.object({
   description: z.string().optional(),
-  doneAt: z.string().datetime().nullable(),
+  doneAt: z.string().datetime().nullable().optional(),
 })
 
 export type UpdateTodoData = z.infer<typeof updateTodoDataSchema>
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -6,24 +6,26 @@ import {
   TsRestRequest,
 } from '@ts-rest/nest'
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator'
+import { contract } from 'src/contract'
 import { UserDb } from 'src/database/drizzle/drizzle.tables.schemas'
 import { MyController } from 'src/utils/decorators/my-controller'
 import { TodoErrorMessage } from './enums/todo-error.message'
+import { DeleteTodoHandler } from './handler/delete-todo/delete-todo.handler'
 import { UpdateTodoHandler } from './handler/update-todo/update-todo.handler'
-import { todoC } from './todo.c'
 import { TodoRepository } from './todo.repository'
 
-const c = nestControllerContract(todoC)
+const c = nestControllerContract(contract.todos)
 
 type ReqShape = NestRequestShapes<typeof c>
 
-type GetUserTodos = ServerInferResponses<typeof todoC.getUserTodos>
+type GetUserTodos = ServerInferResponses<typeof c.getUserTodos>
 
 @MyController()
 export class TodoController {
   constructor(
     private readonly todoRepository: TodoRepository,
     private readonly updateTodoHandler: UpdateTodoHandler,
+    private readonly deleteTodoHandler: DeleteTodoHandler,
   ) {}
 
   @TsRest(c.getUserTodos)
@@ -43,7 +45,7 @@ export class TodoController {
     @CurrentUser()
     user: UserDb,
     @TsRestRequest() { body }: ReqShape['createTodo'],
-  ): Promise<ServerInferResponses<typeof todoC.createTodo>> {
+  ): Promise<ServerInferResponses<typeof c.createTodo>> {
     const createdTodo = await this.todoRepository.createTodo({
       description: body.description,
       userId: user.id,
@@ -60,7 +62,7 @@ export class TodoController {
     @CurrentUser()
     requester: UserDb,
     @TsRestRequest() { body, params }: ReqShape['updateTodo'],
-  ): Promise<ServerInferResponses<typeof todoC.updateTodo>> {
+  ): Promise<ServerInferResponses<typeof c.updateTodo>> {
     const result = await this.updateTodoHandler.try({
       data: body,
       requesterId: requester.id,
@@ -83,6 +85,37 @@ export class TodoController {
 
     return {
       status: 200,
+      body: result.value,
+    }
+  }
+
+  @TsRest(c.deleteTodo)
+  async deleteTodo(
+    @CurrentUser()
+    requester: UserDb,
+    @TsRestRequest() { params }: ReqShape['deleteTodo'],
+  ): Promise<ServerInferResponses<typeof c.deleteTodo>> {
+    const result = await this.deleteTodoHandler.try({
+      requesterId: requester.id,
+      todoId: params.todoId,
+    })
+
+    if (result.isErr()) {
+      if (result.error === TodoErrorMessage.TODO_NOT_FOUND) {
+        return {
+          status: 404,
+          body: result.error,
+        }
+      }
+
+      return {
+        status: 403,
+        body: result.error,
+      }
+    }
+
+    return {
+      status: 204,
       body: result.value,
     }
   }
